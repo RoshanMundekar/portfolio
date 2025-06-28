dfa2e08f0ecac960786dc3e90e1bbf2e4ba28d3d
Roshan Mundekar <155573532+roshan501@users.noreply.github.com>
2025-06-28 19:34:59 +0530

    upload

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+/* typing animation*/
+var typed = new Typed(".typing",{
+    strings:["Software Developer Engineer","Python Backend Engineer", "Python Developer", "Data Scientist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
